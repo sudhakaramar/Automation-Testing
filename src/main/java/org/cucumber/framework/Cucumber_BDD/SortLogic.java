5166eadcf844b2e17f6ab99f8193d1c154e82388
sudhakarED <41256031+sudhakarED@users.noreply.github.com>
2020-01-18 11:29:09 +0530

    removed

--- src/main/java/org/cucumber/framework/Cucumber_BDD/SortLogic.java
+++ src/main/java/org/cucumber/framework/Cucumber_BDD/SortLogic.java
@@ -8,8 +8,7 @@ import java.util.Arrays;
 public class SortLogic {
 	
 	public static void main(String[] args) throws FileNotFoundException {
-	System.out.println("in");
-	System.out.println("in");
+	
 		
 		
 	}
