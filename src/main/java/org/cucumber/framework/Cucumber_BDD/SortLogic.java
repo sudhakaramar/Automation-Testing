60eb2fc15011636e1eda1757346db238dc257a3b
sudhakarED <41256031+sudhakarED@users.noreply.github.com>
2020-01-18 14:33:33 +0530

    a

--- src/main/java/org/cucumber/framework/Cucumber_BDD/SortLogic.java
+++ src/main/java/org/cucumber/framework/Cucumber_BDD/SortLogic.java
@@ -11,12 +11,7 @@ public class SortLogic {
 	public static void main(String[] args) throws FileNotFoundException{
 
 		System.out.println("branch1");
-		System.out.println("resetting");
-		
-
-
-
-	}
+}
 
 }
 
